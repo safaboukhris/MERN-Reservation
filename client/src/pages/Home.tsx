1b775dcb735fe46b55c68c1618153b50df055791
marwa <boukhrismarwa06@gmail.com>
2024-12-12 10:14:45 +0100

    api-front

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -9,17 +9,7 @@ const Home = () => {
     <>
       <div className="relative h-[800px]  bg-cover" style={{ backgroundImage: "url('/coworking7.jpg')" ,  backgroundRepeat: 'no-repeat'}}>
         <Header />
-        <div className="absolute inset-0 flex justify-center items-center text-white">
-          <div className="text-center">
-            <p className="text-5xl text-white font-[YujiMai] font-bold">
-              <span className="text-[#ff861a]">UN MEILLEUR ENVIRONEMENT </span> POUR TRAVAILLER
-            </p>
-            <p className="text-lg mt-8 font-[YujiMai] text-white font-bold">
-              Le partage et l'échange sont notre devise. Rejoignez notre communauté et tentez l'expérience{" "}
-              <span className="text-[#ff861a] font-extrabold">RB coworking space...</span>
-            </p>
-          </div>
-        </div>
+        
       </div>
       <BoxInfo />
       <div className="mt-[17%]">
